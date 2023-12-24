1c5b73820209587e6702809f9bc601d30a6fd8dd
Bách Trần <bachtran10013@gmail.com>
2023-12-24 16:46:51 +0700

    if else

---
+++ ex-6-if-else.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	i := 4
+	if i > 5 {
+		println("great than 5")
+	} else if i > 2 {
+		println("great than 2 and less than 5")
+	} else {
+		println("less than 2")
+	}
+}
